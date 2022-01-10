f60afb442af8b60e7353f8005c7358ed37e2c423
Bikash <kacperlitwinow517@gmail.com>
2022-01-10 14:42:33 +0100

    Change Path character

--- app/src/main/java/com/example/finalspaceapi/data/remote/FinalSpaceApi.kt
+++ app/src/main/java/com/example/finalspaceapi/data/remote/FinalSpaceApi.kt
@@ -3,6 +3,7 @@ package com.example.finalspaceapi.data.remote
 import com.example.finalspaceapi.data.remote.dto.DetailsCharacter
 import com.example.finalspaceapi.data.remote.dto.FinalSpaceItemDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FinalSpaceApi {
@@ -11,6 +12,6 @@ interface FinalSpaceApi {
     suspend fun getFinalSpaceList(): List<FinalSpaceItemDto>
 
     @GET("/api/v0/character/{id}")
-    suspend fun getCharacterDetails(@Query("id") id: Int): DetailsCharacter
+    suspend fun getCharacterDetails(@Path("id") id: Int): DetailsCharacter
 
 }
